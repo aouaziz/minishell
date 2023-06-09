ac1e361af48c69e43de126664a0b86bbec583283
kertgbh-nqt <kido9odik@gmail.com>
2023-06-09 07:59:32 +0100

    add header / #include

--- utils/ftt_isdigit.c
+++ utils/ftt_isdigit.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ftt_strdup.c                                       :+:      :+:    :+:   */
+/*   ftt_isdigit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-garr <mel-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/09 07:58:42 by mel-garr          #+#    #+#             */
-/*   Updated: 2023/06/09 07:58:43 by mel-garr         ###   ########.fr       */
+/*   Created: 2023/06/09 07:57:39 by mel-garr          #+#    #+#             */
+/*   Updated: 2023/06/09 07:58:26 by mel-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/mini.h"
 
-char *ftt_strdup(char *str)
+int  ftt_isdigit(char c)
 {
-  int i;
-  int len;
-  char *dup;
-  
-  i = 0;
-  len = ftt_strlen(str);
-  dup = (char *)malloc(sizeof(char) * len + 1);
-  if (!dup)
-    return (NULL);
-  while (str[i])
-  {
-    dup[i] = str[i];
-    i++;
-  }
-  dup[i] = '\0';
-  return (dup);
+  if (c >= '0' && c <= '9')
+    return (1);
+  return (0);
 }
--- utils/ftt_isdit.c
+++
@@ -1,8 +0,0 @@
-#include "../mini.h"
-
-int  ftt_isdigit(char c)
-{
-  if (c >= '0' && c <= '9')
-    return (1);
-  return (0);
-}
